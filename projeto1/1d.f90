05238b4f51c3f1b5a7942ab1f6605995d4ca7972
Heitor Pascoal de Bittencourt <heitorPB@users.noreply.github.com>
2018-02-06 15:24:19 -0200

    [P1 - Eric] item D adicionado

---
+++ projeto1/1d.f90
@@ -0,0 +1,52 @@
+program ordena
+        implicit none
+
+        integer :: N, M, i
+        real, allocatable, dimension(:) :: lista
+
+        print *, "Enter N"
+        read *, N
+        print *, "Enter M"
+        read *, M
+
+        if (N < 1) then
+                print *, "Stupid value of N detected"
+                stop "Bye"
+        end if
+
+        if (M > N .or. M < 0) then
+                print *, "Stupid value of M detected"
+                print *, "Setting M = N"
+                M = N
+        end if
+
+        allocate(lista(N))
+        read *, (lista(i), i = 1, N)
+
+        call InsertionSort(lista, N, M)
+        print *, (lista(i), i = 1, M)
+
+        deallocate(lista)
+
+
+contains
+subroutine InsertionSort(list, N, M)
+! https://en.wikipedia.org/wiki/Insertion_sort
+! https://github.com/frigaut/yorick-imutil/blob/master/insort.c
+        real, intent(inout) :: list(N)
+        integer, intent(in) :: N, M
+
+        integer :: i, j
+        real :: temp
+
+        do i = 1, M
+                j = i
+                temp = list(j)
+                do while ( j > 1 .AND. list(j - 1) > temp)
+                        list(j) = list(j - 1)
+                        j = j - 1
+                end do
+                list(j) = temp
+        end do
+end subroutine InsertionSort
+end program ordena
